d3808d488be93c6f21fbbd3762a3f1640233b7b7
akankshar1210 <akanksha.xworkz@gmail.com>
2023-06-20 20:14:31 +0530

    test-push

---
+++ Datatypes.java
@@ -0,0 +1,105 @@
+public class Datatypes{
+	public static void main(String[] args){
+		byte totalNumberOfStates=29;
+		byte heightOfPerson=6;
+		byte totalNumberOfMonths=12;
+		byte totalNumberOfDaysInWeek=7;
+		byte roomTemperature=35;
+		byte sidesOfSquare=6;
+		byte totalNumberOfVowels=5;
+		byte totalNumberOfAlphabets=26;
+		byte personAge=30;
+		
+		short pGRent=10000;
+		short costOfGoldInGrams=5000;
+		short distanceFromBangaloreToManali=2000;
+		short distanceFromBangaloreToBelgaum=510;
+		short totalNumberOfStatesInWorld=195;
+		
+		int distanceSunAndEarth=1500000000;
+		int populationOfIndia=1420000000;
+		int distanceBetweenEarthAndMoon=384400000;
+		
+		long worldPopulation=20000000000L;
+		long phoneNumber=7259278761L;
+		long aadharNumber=232065432120L;
+		long atmNumber=6785340123L;
+		
+		float gravityOfEarth=9.8f;
+		float valueOfPie=3.142f;
+		float cgpa=8.4f;
+		float heightOfQutubMinar=72.5f;
+		
+		
+		double speedOfLight=300.76567897821D;
+		
+		
+		char grade='A';
+		char neutralSign='N';
+		char size='S';
+		char division='D';
+		char naturalNumber='N';
+		char firstLetterOfName='A';
+		char gravitySymbol='G';
+		char speed='S';
+		char distance='D';
+		
+		boolean todayIsFriday=true;
+		boolean isItRain=false;
+		boolean isJavaUnderstanding=false;
+		boolean qutubMinarIsSecondTallest=true;
+		boolean sundayIsHoliday=true;
+		boolean thereIsNoClassTomorrow=false;
+		boolean hadYourLunch=false;
+		boolean isItCold=true;
+		boolean rainbowHasSevenColours=true;
+		boolean thereAreSevenVowels=false;
+		
+		System.out.println(totalNumberOfStates);
+		System.out.println(heightOfPerson);
+		System.out.println(totalNumberOfMonths);
+		System.out.println(totalNumberOfDaysInWeek);
+		System.out.println(roomTemperature);
+		System.out.println(sidesOfSquare);
+		System.out.println(totalNumberOfVowels);
+		System.out.println(totalNumberOfAlphabets);
+		System.out.println(personAge);
+		System.out.println(pGRent);
+		System.out.println(costOfGoldInGrams);
+		System.out.println(distanceFromBangaloreToManali);
+		System.out.println(distanceFromBangaloreToBelgaum);
+		System.out.println(totalNumberOfStatesInWorld);
+		System.out.println(distanceSunAndEarth);
+		System.out.println(populationOfIndia);
+		System.out.println(distanceBetweenEarthAndMoon);
+		System.out.println(worldPopulation);
+		System.out.println(phoneNumber);
+		System.out.println(aadharNumber);
+		System.out.println(atmNumber);
+		System.out.println(gravityOfEarth);
+		System.out.println(valueOfPie);
+		System.out.println(cgpa);
+		System.out.println(heightOfQutubMinar);
+		System.out.println(speedOfLight);
+		System.out.println(grade);
+		System.out.println(neutralSign);
+		System.out.println(size);
+		System.out.println(division);
+		System.out.println(naturalNumber);
+		System.out.println(firstLetterOfName);
+		System.out.println(gravitySymbol);
+		System.out.println(speed);
+		System.out.println(distance);
+		System.out.println(todayIsFriday);
+		System.out.println(isItRain);
+		System.out.println(isJavaUnderstanding);
+		System.out.println(qutubMinarIsSecondTallest);
+		System.out.println(sundayIsHoliday);
+		System.out.println(thereIsNoClassTomorrow);
+		System.out.println(hadYourLunch);
+		System.out.println(isItCold);
+		System.out.println(rainbowHasSevenColours);
+		System.out.println(thereAreSevenVowels);
+	
+	}
+}
